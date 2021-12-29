32a2b8d45257189467755781708522e4838fa5bb
Alaeddine TELLI <telliala@gmail.com>
2021-12-29 17:27:02 +0100

    make tests

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-
 from DbConnect import DBConnect
 from InvoiceObj import Client,Product,Invoice,InvoiceItem
 from datetime import date
+
+
+#create Clients objects
+ala=Client("Alaeddine","0664661955","contact@telliala.info","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
+baha=Client("Bahaeddine","0778604044","contact@tellibaha.info","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
+moh=Client("Mohammed","0777776648","tellimohammed@gmail.com","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
+dbop=DBConnect()
+
+#add clients to db
+dbop.add_client(ala)
+dbop.add_client(baha)
+dbop.add_client(moh)
+
+#fetch all clients
+
+dbop.get_all_clients()
 
 
 
