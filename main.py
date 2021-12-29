79102bfc56fd3904d49e84873a67a0252f1b2d3d
Alaeddine TELLI <telliala@gmail.com>
2021-12-29 17:20:59 +0100

    fix the main

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from Client import Client
+
 from DbConnect import DBConnect
+from InvoiceObj import Client,Product,Invoice,InvoiceItem
 from datetime import date
 
 
-client1=Client("bahaeddine","0664661955","telliala@gmail.com","BP 198 ROUISSAT OUARGLA",date.today())
-client2=Client("ala","066466955","teliala@gmail.com","BP 198 ROUISSAT OUARGLA",date.today())
-client3=Client("baha","06641955","tellla@gmail.com","BP 198 ROUISSAT OUARGLA",date.today())
 
-dbConnect=DBConnect()
-# dbConnect.add_client(client1)
-# dbConnect.add_client(client2)
-# dbConnect.add_client(client3)
-# client=dbConnect.get_client(2)
-#dbConnect.get_all_clients()
-client4=Client('bahaeddine',"06686695","ala@ala.co","rouissat",date.today())
-dbConnect.update_client(client4,2)
-client=dbConnect.get_client(2)
 
 
 
