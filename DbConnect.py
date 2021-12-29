8834516a05e342a0b5f674048861a881d2e7c9bd
Alaeddine TELLI <telliala@gmail.com>
2021-12-29 17:51:59 +0100

    add product operations

--- DbConnect.py
+++ DbConnect.py
@@ -3,12 +3,8 @@ import sqlite3
 
 class DBConnect :
 
-    def __init__(self):
-        self.connect_db()
-
-    def connect_db(self):
-        pass
     
+    #Client operations
     def add_client(self,client):
         try:
             self.db=sqlite3.connect("invoicesDB.db")
@@ -35,6 +31,7 @@ class DBConnect :
             
         except self.db.Error as error:
             print('faild to get clients',error)
+
     def update_client(self,client,id):
         try:
             self.db=sqlite3.connect("invoicesDB.db")
@@ -71,6 +68,76 @@ class DBConnect :
                 print("Email: ", row[3])
                 print("address: ", row[4])
                 print("joinDate: ", row[5])
+                print("\n")
+            cursor.close()
+            self.db.close() 
+            
+        except self.db.Error as error:
+            print('faild to get clients',error)
+
+    #product operations
+    def add_product(self,product):
+        try:
+            self.db=sqlite3.connect("invoicesDB.db")
+            self.db.row_factory=sqlite3.Row
+            cursor=self.db.cursor()
+            self.db.execute("insert into  Product (ProductName, ProductPrice, ProductCreated_At,ProductUpdated_At) values(?,?,?,?)",
+                        (product.name,product.price,product.create,product.update))
+            self.db.commit()
+            cursor.close()
+            self.db.close()
+        except self.db.Error as error:
+            print(error)
+
+    def get_product(self,id):
+        try:
+            self.db=sqlite3.connect("invoicesDB.db")
+            cursor=self.db.cursor()
+            cursor.execute(""" SELECT * from Product where ProductID = ? """,(id,))
+            clients=cursor.fetchone()
+            for client in clients:
+                print(client)
+            cursor.close()
+            self.db.close() 
+            
+        except self.db.Error as error:
+            print('faild to get clients',error)
+
+    def update_product(self,product):
+        try:
+            self.db=sqlite3.connect("invoicesDB.db")
+            cursor=self.db.cursor()
+            self.db.execute("""Update Client set ProductName = ?,ProductPrice=?, ProductCreated_At = ?,ProductUpdated_At=? WHERE ProductID = ?""",
+                            (product.name,product.price,product.create,product.update,id))
+            self.db.commit()
+            cursor.close()
+            self.db.close() 
+        except self.db.Error as error:
+            print('faild to update ',client.name,'information',error)
+
+    def remove_product(self,id):
+        try:
+            self.db=sqlite3.connect("invoicesDB.db")
+            cursor=self.db.cursor()
+            self.db.execute("""DELETE FROM Product WHERE ProductID = ?""",(id,))
+            
+            cursor.close()
+            self.db.close() 
+        except self.db.Error as error:
+            print('faild to remove elemn',error)
+
+    def get_all_products(self):
+        try:
+            self.db=sqlite3.connect("invoicesDB.db")
+            cursor=self.db.cursor()
+            cursor.execute(""" SELECT * from Product """)
+            clients=cursor.fetchall()
+            for row in clients:
+                print("Id: ", row[0])
+                print("Name: ", row[1])
+                print("price: ", row[2])
+                print("create: ", row[3])
+                print("update: ", row[4])
                 print("\n")
             cursor.close()
             self.db.close() 
--- main.py
+++ main.py
@@ -5,18 +5,23 @@ from datetime import date
 
 #create Clients objects
 ala=Client("Alaeddine","0664661955","contact@telliala.info","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
-baha=Client("Bahaeddine","0778604044","contact@tellibaha.info","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
-moh=Client("Mohammed","0777776648","tellimohammed@gmail.com","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
+# baha=Client("Bahaeddine","0778604044","contact@tellibaha.info","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
+# moh=Client("Mohammed","0777776648","tellimohammed@gmail.com","BP 198 ROUISSAT OUARGLA ALGERIA",date.today())
+
+product1=Product("iphone",256.000,date.today(),date.today())
+invoice1=Invoice("ala-2563",ala,False,date.today(),date.today())
+invoice_item=InvoiceItem(ala,invoice1,5)
 dbop=DBConnect()
 
 #add clients to db
-dbop.add_client(ala)
-dbop.add_client(baha)
-dbop.add_client(moh)
+# dbop.add_client(ala)
+# dbop.add_client(baha)
+# dbop.add_client(moh)
 
 #fetch all clients
 
-dbop.get_all_clients()
+#dbop.get_all_clients()
+
 
 
 
